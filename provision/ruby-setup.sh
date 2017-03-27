2d9568893f56e69756a332c2d7bcf94fda752c54
Csaba Apagyi <csaba.apagyi@gmail.com>
2017-03-28 00:10:12 +0200

    Fix installation with umake; add rvm & ruby installation

--- provision/ruby-setup.sh
+++ provision/ruby-setup.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-umake ide rubymine
+yes '\n' | umake ide rubymine
+
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+\curl -sSL https://get.rvm.io | bash -s stable
+
+source ~/.rvm/scripts/rvm
+
+rvm install ruby --latest
+bash --login
+rvm use ruby --latest
