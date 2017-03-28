101aa4d08631a5182b6247161193e7955f84bae8
Csaba Apagyi <csaba.apagyi@gmail.com>
2017-03-28 17:13:49 +0200

    Fix issue with rvm not being recognized

--- provision/ruby-setup.sh
+++ provision/ruby-setup.sh
@@ -5,7 +5,7 @@ expect -f $(dirname "$0")/ruby-setup-expect.sh
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 \curl -sSL https://get.rvm.io | bash -s stable
 
-source ~/.rvm/scripts/rvm
+source /etc/profile.d/rvm.sh
 
 rvm install ruby --latest
 bash --login
