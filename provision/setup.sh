0b4259fe73c81f71b24d91360a6af09b1518dc8d
Csaba Apagyi <csaba.apagyi@gmail.com>
2017-04-02 14:36:12 +0200

    Add provision time measurement

--- provision/setup.sh
+++ provision/setup.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+source $(dirname "$0")/utils.sh
+
+start=`date +%s`
+
 $(dirname "$0")/general-setup.sh
 $(dirname "$0")/ruby-setup.sh
 $(dirname "$0")/project-setup.sh
 $(dirname "$0")/verify.sh
+
+end=`date +%s`
+runtime=$((end-start))
+echo_headline "Provision executed in $(seconds_to_hh_mm_ss $runtime)"
--- provision/utils.sh
+++ provision/utils.sh
@@ -7,3 +7,5 @@ echo_err() { cat <<< "$@" 1>&2; }
 echo_headline() { printf "\n################\n"; printf "$@"; printf "\n################\n\n"; }
 
 fix_rvm_availability() { source ~/.rvm/scripts/rvm; bash --login; }
+
+seconds_to_hh_mm_ss() { printf '%02dh:%02dm:%02ds\n' $(($@/3600)) $(($@%3600/60)) $(($@%60)); }
