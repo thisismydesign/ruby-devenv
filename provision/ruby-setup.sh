09841e23c9f813353f0ecbe1caa359f052d967fc
Csaba Apagyi <csaba.apagyi@gmail.com>
2017-04-04 15:43:12 +0200

    Install Ruby alongside with RVM in quiet mode

--- provision/ruby-setup.sh
+++ provision/ruby-setup.sh
@@ -14,13 +14,9 @@ unzip /vagrant/settings/ruybmine-default.jar -d ~/.RubyMine*/config
 
 echo_headline "Installing RVM..."
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
-\curl -sSL https://get.rvm.io | bash -s stable --quiet-curl
+\curl -sSL https://get.rvm.io | bash -s stable --ruby --quiet-curl
 
 fix_rvm_availability
-
-echo_headline "Installing latest Ruby..."
-rvm install ruby --latest
-rvm use ruby --latest
 
 echo_headline "Installing bundler..."
 gem install bundler
