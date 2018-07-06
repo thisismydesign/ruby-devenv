d05c1fc46bda0fa0b8fd62b8342909608c7958ae
Csaba Apagyi <csaba.apagyi@gmail.com>
2018-07-06 17:31:49 +0200

    Sets up git to use lf

--- provision/general-setup.sh
+++ provision/general-setup.sh
@@ -18,6 +18,10 @@ sudo service lightdm start
 echo_headline "Installing apps via apt..."
 sudo apt-get install -y tree git ubuntu-make expect nautilus-open-terminal
 
+echo_headline "Setting up git to use LF line endings..."
+# For more info see https://git-scm.com/book/en/v2/Customizing-Git-Git-Configuration#_formatting_and_whitespace
+git config --global core.autocrlf input
+
 echo_headline "Installing apps via umake..."
 expect -c 'spawn umake ide visual-studio-code; expect "Choose installation path:" { send "\r" }; expect "[I Accept (a)" { send "a\r" }; set timeout 900; expect "Installation done"'
 expect -c 'spawn umake ide sublime-text; expect "Choose installation path:" { send "\r" }; set timeout 900; expect "Installation done"'
